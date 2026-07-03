e04023d8ddf54997fe417bba2aa3c5224f17601d
expectexception <expectexceptionIN@users.noreply.github.com>
2026-07-03 20:19:09 +0000

    Completely remove PostgreSQL dependency, switch to SQLite default

--- expectexception/settings.py
+++ expectexception/settings.py
@@ -106,15 +106,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'expectexception.wsgi.application'
 
-# Database: use sqlite by default for dev
+# Database: use sqlite by default
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', 'expectexception'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
